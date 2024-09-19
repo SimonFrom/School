5ad5f9942357f8aba7db24d8ab2429e095a151cf
SimonFrom <simon.bulder@gmail.com>
2024-09-19 21:05:38 +0200

    First commit

---
+++ MenuSystem/MenuSystem/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    public class Menu
+    {
+        public string Title = "";
+        public string[] MenuItem = new string[10];
+        public int ItemCount = 0;
+
+        public MenuItem[] MenuItems;
+    }
+}
---
+++ MenuSystem/MenuSystem/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    public class MenuItem
+    {
+        Menu mainMenu = new Menu();
+        MenuItem mi = new MenuItem();
+        /*public string Show(string mainMenu, string mi)
+        {
+            Console.WriteLine(mainMenu.Title);
+            return mainMenu;
+        }*/
+        public string Title = "";
+        
+    }
+}
